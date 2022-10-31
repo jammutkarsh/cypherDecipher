ee2bdc9a4ae9b3fa6e33338f32b35d4e325bbb74
JammUtkarsh <5.utkarshchourasia@gmail.com>
2022-10-31 16:13:34 +0530

    fix: module naming again

--- cypher.go
+++ cypher.go
@@ -1,25 +1,24 @@
-/*The main objective of this package is to learn
-1. How to build conflict free packages
-2. How to publish packages
-3. How to use `git tag` and use them to have a versioned release of your package.
-*/
-
 // Package cypherDecipher contains functions for adding and removing salt from the text.
 package cypherDecipher
 
 import (
+	"unicode/utf8"
+
 	"github.com/tjarratt/babble"
 )
 
 // CypherPassword adds salt to the password. It takes in a normal string and returns a salted string and the length of the salt.
-func CypherPassword(password string) (saltedPassword string, spCount int) {
+func CypherPassword(password string) (saltedPassword string, pCount int, spCount int) {
 	word := randomWordGenerator()
-	return password[:len(password)/2] + word + password[len(password)/2:], len(word)
+	pCount = utf8.RuneCountInString(password)
+	saltedPassword = password[:utf8.RuneCountInString(password)/2] + word + password[utf8.RuneCountInString(password)/2:]
+	spCount = utf8.RuneCountInString(saltedPassword)
+	return
 }
 
 // DecipherPassword removes the salt from the password. It takes in a salted string, the length of the original password and length of the salt and returns the original password.
 func DecipherPassword(saltedPassword string, pCount, spCount int) string {
-	return saltedPassword[:spCount/2] + saltedPassword[pCount/2+spCount:]
+	return saltedPassword[:pCount/2] + saltedPassword[pCount/2+spCount-pCount:]
 }
 
 // randomWordGenerator returns a random word from the default unix dictionary(Works for Windows users too.)
--- go.mod
+++ go.mod
@@ -3,3 +3,8 @@ module github.com/JammUtkarsh/cypherDecipher
 go 1.19
 
 require github.com/tjarratt/babble v0.0.0-20210505082055-cbca2a4833c1
+
+require (
+	github.com/onsi/ginkgo v1.16.5 // indirect
+	github.com/onsi/gomega v1.23.0 // indirect
+)
